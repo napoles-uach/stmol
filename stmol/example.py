81dd4e4081c1bc077bf2b5c729a327097baa12ba
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-01-05 16:58:26 -0700

    Create example.py

---
+++ stmol/example.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from __init__ import showmol
+import py3Dmol
+st.sidebar.title('Show Proteins')
+prot_str='1A2C,1BML,1D5M,1D5X,1D5Z,1D6E,1DEE,1E9F,1FC2,1FCC,1G4U,1GZS,1HE1,1HEZ,1HQR,1HXY,1IBX,1JBU,1JWM,1JWS'
+prot_list=prot_str.split(',')
+bcolor = st.sidebar.color_picker('Pick A Color', '#00f900')
+protein=st.sidebar.selectbox('select protein',prot_list)
+style = st.sidebar.selectbox('style',['line','cross','stick','sphere','cartoon','clicksphere'])
+xyzview = py3Dmol.view(query='pdb:'+protein)
+xyzview.setStyle({style:{'color':'spectrum'}})
+xyzview.setBackgroundColor(bcolor)
+#html_mol = xyzview._make_html()
+showmol(xyzview, height = 500,width=800)
