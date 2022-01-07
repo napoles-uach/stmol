63b505be2d3ec746f4e5c99053e18d5be7d0f1f5
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-01-06 18:06:23 -0700

    Update example.py

--- stmol/example.py
+++ stmol/example.py
@@ -10,5 +10,4 @@ style = st.sidebar.selectbox('style',['line','cross','stick','sphere','cartoon',
 xyzview = py3Dmol.view(query='pdb:'+protein)
 xyzview.setStyle({style:{'color':'spectrum'}})
 xyzview.setBackgroundColor(bcolor)
-#html_mol = xyzview._make_html()
 showmol(xyzview, height = 500,width=800)
