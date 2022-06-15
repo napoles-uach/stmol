3c52cd92055aa82c274af88a6c3561aad2322b62
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-06-14 18:00:56 -0600

    Update MyComponent.tsx

--- stmol/frontend/src/MyComponent.tsx
+++ stmol/frontend/src/MyComponent.tsx
@@ -1,54 +1,70 @@
-import React, { ReactNode } from "react"
 import {
-  withStreamlitConnection,
-  StreamlitComponentBase,
   Streamlit,
-} from "./streamlit"
+  StreamlitComponentBase,
+  withStreamlitConnection,
+} from "streamlit-component-lib"
+import React, { ReactNode } from "react"
 
-interface State {
-  numClicks: number
-}
+//import 3dmol from 3dmol
+//import $3Dmol from "3dmol"
+//import {$3Dmol} from "3dmol"
 
-/**
- * This is a React-based component template. The `render()` function is called
- * automatically when your component should be re-rendered.
- */
-class MyComponent extends StreamlitComponentBase<State> {
-  public state = { numClicks: 0 }
 
+class MyComponent extends StreamlitComponentBase<any> {
   public render = (): ReactNode => {
-    // Arguments that are passed to the plugin in Python are accessible
-    // via `this.props.args`. Here, we access the "name" arg.
-    const name = this.props.args["name"]
 
-    // Show a button and some text.
-    // When the button is clicked, we'll increment our "numClicks" state
-    // variable, and send its new value back to Streamlit, where it'll
-    // be available to the Python program.
-    return (
+    const title = this.props.args["title"]
+    const subtitle = this.props.args["subtitle"]
+    const body = this.props.args["body"]
+    const link = this.props.args["link"]
+    const color = this.props.args["color"]
+    const Chemiscope = require("chemiscope")
+
+    
+
+ 
+/*    return (
       <span>
-        Hello, {name}! &nbsp;
-        <button onClick={this.onClicked} disabled={this.props.disabled}>
-          Click Me!
-        </button>
+        <h1>{title}</h1>
+        <h2>'3dmol'</h2>
+        <h2>{subtitle}</h2>
+        <p>{body}</p>
+        <a href={link}>{link}</a>
+        <script src="https://3Dmol.org/build/3Dmol-min.js" async></script>     
+        <div style={{height: "400px", width: "400px", position: "relative"}} className='viewer_3Dmoljs' data-pdb='2POR' data-backgroundcolor='0xffffff' data-style='stick' ></div>
+        
+      </span>
+      
+    ) */
+
+    return (
+      
+    
+      <span>
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+        <p>{body}</p>
+        <a href={link}>{link}</a>
+
+
+        <script src="https://3Dmol.org/build/3Dmol-min.js" async></script>
+     
+
+        <div style={{height: "400px", width: "400px", position: "relative"}} className='viewer_3Dmoljs' data-pdb='2POR' data-backgroundcolor="red" data-style='stick' >
+        
+
+      
+
+        </div>
+
+        
+
       </span>
     )
+
   }
 
-  /** Click handler for our "Click Me!" button. */
-  private onClicked = (): void => {
-    // Increment state.numClicks, and pass the new value back to
-    // Streamlit via `Streamlit.setComponentValue`.
-    this.setState(
-      prevState => ({ numClicks: prevState.numClicks + 1 }),
-      () => Streamlit.setComponentValue(this.state.numClicks)
-    )
-  }
+
 }
 
-// "withStreamlitConnection" is a wrapper function. It bootstraps the
-// connection between your component and the Streamlit app, and handles
-// passing arguments from Python -> Component.
-//
-// You don't need to edit withStreamlitConnection (but you're welcome to!).
 export default withStreamlitConnection(MyComponent)
