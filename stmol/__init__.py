4bff7eaa40b67ea02f7b6ffb2b52e6306530797a
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-06-14 18:12:34 -0600

    added bidirectional code

--- stmol/__init__.py
+++ stmol/__init__.py
@@ -1,3 +1,4 @@
+import os
 import streamlit.components.v1 as components
 
 def showmol(mol_obj,height=500,width=500):
@@ -157,3 +158,80 @@ def component_3dmol(width=800, height=800):
 </body>
 ''', width=width, height=height)
     return 
+
+########### The part below is for making the component bidirectional
+########### In may not interfere with the code above
+
+
+
+# Create a _RELEASE constant. We'll set this to False while we're developing
+# the component, and True when we're ready to package and distribute it.
+# (This is, of course, optional - there are innumerable ways to manage your
+# release process.)
+_RELEASE = False
+
+# Declare a Streamlit component. `declare_component` returns a function
+# that is used to create instances of the component. We're naming this
+# function "_component_func", with an underscore prefix, because we don't want
+# to expose it directly to users. Instead, we will create a custom wrapper
+# function, below, that will serve as our component's public API.
+
+# It's worth noting that this call to `declare_component` is the
+# *only thing* you need to do to create the binding between Streamlit and
+# your component frontend. Everything else we do in this file is simply a
+# best practice.
+
+if not _RELEASE:
+    _component_func = components.declare_component(
+        # We give the component a simple, descriptive name ("my_component"
+        # does not fit this bill, so please choose something better for your
+        # own component :)
+        "my_component",
+        # Pass `url` here to tell Streamlit that the component will be served
+        # by the local dev server that you run via `npm run start`.
+        # (This is useful while your component is in development.)
+        url="http://localhost:3001",
+    )
+else:
+    # When we're distributing a production version of the component, we'll
+    # replace the `url` param with `path`, and point it to to the component's
+    # build directory:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _component_func = components.declare_component("my_component", path=build_dir)
+
+
+# Create a wrapper function for the component. This is an optional
+# best practice - we could simply expose the component function returned by
+# `declare_component` and call it done. The wrapper allows us to customize
+# our component's API: we can pre-process its input args, post-process its
+# output value, and add a docstring for users.
+def my_component(title,subtitle,body,link,color):
+    """Create a new instance of "my_component".
+ """
+    # Call through to our private component function. Arguments we pass here
+    # will be sent to the frontend, where they'll be available in an "args"
+    # dictionary.
+    #
+    # "default" is a special argument that specifies the initial return
+    # value of the component before the user has interacted with it.
+    _ = _component_func(
+        title=title,subtitle=subtitle,body=body,link=link,color=color)
+    
+    
+
+
+
+
+# Add some test code to play with the component while it's in development.
+# During development, we can run this just as we would any other Streamlit
+# app: `$ streamlit run my_component/__init__.py`
+if not _RELEASE:
+    import streamlit as st
+
+
+    num_clicks = my_component("Title1","Subtitle","some random text","https://www.google.com","blue")
+    
+
+
+
