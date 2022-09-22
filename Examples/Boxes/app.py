b1d60fb0336e2c990fb144b3c2e10ccc963a8521
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2022-09-22 10:52:30 -0600

    Update app.py

--- Examples/Boxes/app.py
+++ Examples/Boxes/app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-from stmol import py3Dmol, showmol,add_box
+from stmol import showmol,add_box
 import py3Dmol
-scene = py3Dmol.view(width=800,height=800)
-add_box(scene)
-add_box(scene,bxc=[5,5,5],boxColor='#CD5C5C',boxOpacity=1.0)
-scene.zoom(0.2)
-showmol(scene,width=800,height=800)
+scene = py3Dmol.view(width=800,height=800) # create the py3Dmol object (empty)
+add_box(scene) # adds a box to the scene object with default values
+add_box(scene,bxc=[5,5,5],boxColor='#CD5C5C',boxOpacity=1.0) # add a box with custom values
+scene.zoom(0.2) # zoom
+showmol(scene,width=800,height=800) #This renders the scene in the app
